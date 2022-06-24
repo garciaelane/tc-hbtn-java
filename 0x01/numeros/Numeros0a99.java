b06c63a07a1365380376008c67477fcf56cf2891
Elane Garcia <elanegarcia@ciandt.com>
2022-06-24 14:29:28 -0300

    “texto”

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+
+public class Numeros0a99 {
+    public static void main (String [] args) {
+        for (int = 0; i < 100; i++) {
+            if (i == 99)
+                System.out.print (i + "\n");
+            else
+                System.out.print (i + ", ");
+        }
+
+    }
+}
+
