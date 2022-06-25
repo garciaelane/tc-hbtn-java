c746030f7a8564b18e87d0e8f2cba42e3ab5112e
Elane Garcia <elanegarcia@ciandt.com>
2022-06-25 19:11:13 -0300

    Task 1

---
+++ 0x01/numeros/NumerosPares.java
@@ -0,0 +1,12 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                if (i == 98)
+                    System.out.print(i + "\n");
+                else
+                    System.out.print(i + ", ");
+            }
+        }
+    }
+}
