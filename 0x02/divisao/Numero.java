13617ac931f774f5bf5a8537025bdecd768f01b2
Elane Garcia <elanegarcia@ciandt.com>
2022-06-25 21:42:27 -0300

    Task 0

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -1,11 +1,11 @@
 public class Numero {
-    public class void dividir(inta, int b) {
+    public class void dividir(int a, int b) {
         int resultado = 0;
 
         try {
             resultado = a / b;
         } catch (Exceptiom ex) {
-            System.out.println("Não é possível dividir por zerp");
+            System.out.println("Nao eh possivel dividir por zero");
         } finally {
             System.out.printf("%d / %d = %d\n", a, b, resultado);
         }
