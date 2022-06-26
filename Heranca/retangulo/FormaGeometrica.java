e9aad99baba870c51bea79eb833abe3fbf86a932
Elane Garcia <elanegarcia@ciandt.com>
2022-06-26 18:32:14 -0300

    Task 2

--- Heranca/retangulo/FormaGeometrica.java
+++ Heranca/retangulo/FormaGeometrica.java
@@ -1,6 +1,6 @@
 public class FormaGeometrica {
     public double area() throws UnsupportedOperationException {
-        throw new UnsupportedOperationException ("Funcao Area nao implementada");
+        throw new UnsupportedOperationException ("Funcao area nao implementada");
     }
 }
 
