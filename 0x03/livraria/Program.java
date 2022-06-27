a438367a8a5ae2241e8378abf20467329fa51f2e
Elane Garcia <elanegarcia@ciandt.com>
2022-06-27 13:56:13 -0300

    Task 1

--- 0x03/livraria/Program.java
+++ 0x03/livraria/Program.java
@@ -2,6 +2,7 @@ import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
 
         try {
             Livro livro1 = new Livro(
