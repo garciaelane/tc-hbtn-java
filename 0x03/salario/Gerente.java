2284a400c046e1745a96b75d656663b83ec587c9
Elane Garcia <elanegarcia@ciandt.com>
2022-06-26 22:15:38 -0300

    Task 0

--- 0x03/salario/Gerente.java
+++ 0x03/salario/Gerente.java
@@ -7,10 +7,10 @@ public class Gerente extends Empregado {
     @Override
     public double calcularBonus(Departamento departamento){
         if (departamento.alcancouMeta()){
-            return (salarioFixo*0,2 +
-                    ((departamento.getValorAtingidoMeta()- departamento.getValorMeta())*0.01));
+            return (salarioFixo * 0.2 +
+                    ((departamento.getValorAtingidoMeta()- departamento.getValorMeta()) * 0.01));
         } else{
-            return 0,00f;
+            return 0.00f;
         }
     }
 }
