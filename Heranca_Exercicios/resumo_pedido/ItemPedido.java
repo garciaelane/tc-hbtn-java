c084710636f8aea4b1173b1748d595e3cddfa1ff
Elane Garcia <elanegarcia@ciandt.com>
2022-06-28 20:14:56 -0300

    Task 1

---
+++ Heranca_Exercicios/resumo_pedido/ItemPedido.java
@@ -0,0 +1,23 @@
+package resumo_pedido;
+
+import produtos.Produto;
+
+public class ItemPedido {
+    public int quantidade;
+    public Produto produto;
+
+    public ItemPedido(Produto produto, int quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+}
+
