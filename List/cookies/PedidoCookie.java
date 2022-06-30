e38478b5208a0256128cecee9446f2e6f01de165
Elane Garcia <elanegarcia@ciandt.com>
2022-06-29 23:16:47 -0300

    Task 1

--- List/cookies/PedidoCookie.java
+++ List/cookies/PedidoCookie.java
@@ -1,4 +1,26 @@
-package cookies;
-
 public class PedidoCookie {
+    String sabor;
+    int quantidadeCaixas;
+
+    public PedidoCookie(String sabor, int quantidadeCaixas) {
+        this.sabor = sabor;
+        this.quantidadeCaixas = quantidadeCaixas;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public int getQuantidadeCaixas() {
+        return quantidadeCaixas;
+    }
+
+    public void setQuantidadeCaixas(int quantidadeCaixas) {
+        this.quantidadeCaixas = quantidadeCaixas;
+    }
 }
+
